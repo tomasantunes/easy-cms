f7ad001687d47eaa9600bd681110dd106d219d44
Tomás Antunes <tomasantunes@gmail.com>
2018-12-31 14:34:08 +0000

    update login

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 
@@ -10,6 +10,7 @@ db = connect_db()
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.secret_key = "default_key"
 
 def init():
 	sql_users_table = """ CREATE TABLE IF NOT EXISTS users (
@@ -28,8 +29,10 @@ def home():
 
 @app.route("/admin")
 def admin():
-	if (app.config["LOGIN"] == True):
-		return "admin page"
+	print(str(session))
+	if 'username' in session:
+		username = session['username']
+		return 'Logged in as ' + username + '<br>' + "<b><a href = '/logout'>click here to log out</a></b>"
 	else:
 		return render_template("login.html")
 
@@ -39,21 +42,27 @@ def register():
 
 @app.route("/add_user", methods=['POST'])
 def add_user():
-	username = request.args.get('username', "")
-	email = request.args.get('email', "")
-	password = request.args.get('password', "")
+	username = request.form.get('username', "")
+	email = request.form.get('email', "")
+	password = request.form.get('password', "")
 
-	db = connect_db()
-	db.execute('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, generate_password_hash(password)])
-	db.commit()
+	print(username)
+	print(email)
+	print(password)
 
-	return jsonify({"status": "OK"})
+	if (username != "" and email != "" and password != ""):
+		db = connect_db()
+		db.execute('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, generate_password_hash(password)])
+		db.commit()
+		return redirect("/admin")
+	return redirect("/register")
+	
 
 @app.route("/auth", methods=['POST'])
 def auth():
 	db = connect_db()
-	username = request.args.get('username', "")
-	password = request.args.get('password', "")
+	username = request.form.get('username', "")
+	password = request.form.get('password', "")
 
 	c = db.execute('SELECT * FROM users WHERE username = ?', [username])
 	users = c.fetchall()
@@ -61,10 +70,17 @@ def auth():
 	if (len(users) > 0):
 		user = users[0]
 		if (check_password_hash(user[3], password)):
-			app.config["LOGIN"] = True
-			return jsonify({"status": "OK"})
+			session['username'] = username
+			print(username)
+			print(str(session))
+			return redirect("/admin")
 	
-	return jsonify({"status": "ERROR"})
+	return redirect("/admin")
+
+@app.route('/logout')
+def logout():
+   session.pop('username', None)
+   return redirect("/admin")
  
 if __name__ == "__main__":
 	init()
